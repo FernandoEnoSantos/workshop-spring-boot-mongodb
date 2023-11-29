0fa5624425744c821178c4f38ab631ba53235a66
Fernando Santos <enobarbosafernando@gmail.com>
2023-11-29 01:27:54 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/fernandosantos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fernandosantos/workshopmongo/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.fernandosantos.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fernandosantos.workshopmongo.domain.User;
+import com.fernandosantos.workshopmongo.dto.UserDTO;
 import com.fernandosantos.workshopmongo.services.UserService;
 
 @RestController
@@ -21,10 +21,11 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET) //poderia ser anotation @GetMapping no lugar
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
-	}
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList()); //1 transforma List<User> em Stream para poder aplicar função Lambda.
+		return ResponseEntity.ok().body(listDTO);													 //2 o .map pega cada obj da lista original e aplica a função, logo para X faça um new UserDTO(X) instancia com o proprio obj				
+	}																								//3 e ai o collect(Collectors.toList() volta para um lista só que agora de DTO
 
 }
