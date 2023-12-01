a8f3974c3b3fe7804b02e6ae9d5cdafcbb4684e6
Fernando Santos <enobarbosafernando@gmail.com>
2023-12-01 13:09:44 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/fernandosantos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fernandosantos/workshopmongo/resources/UserResource.java
@@ -17,7 +17,7 @@ public class UserResource {
 	@Autowired
 	private PostService service;
 	
-																							//3 e ai o collect(Collectors.toList() volta para um lista só que agora de DTO
+																							
 
 	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id) //@PathVariable indica que o id informado no método casa com o informa no Id do requeste, la no value = "/{id}"
