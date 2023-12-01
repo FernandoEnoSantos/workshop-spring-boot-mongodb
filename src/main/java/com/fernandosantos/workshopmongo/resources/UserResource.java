ef805ae82f6ef8d7e9c636b2d9d56b7a208092a3
Fernando Santos <enobarbosafernando@gmail.com>
2023-11-30 22:47:02 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/fernandosantos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fernandosantos/workshopmongo/resources/UserResource.java
@@ -49,5 +49,14 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 		
 	}
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+	public ResponseEntity<Void> Delete(@PathVariable String id) 
+	{
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
 }
 
